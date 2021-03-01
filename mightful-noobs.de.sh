356e0071cc7c56c50f0f9bcb38e6c9f0fd44becd
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-03-01 13:01:09 +0100

    Update mightful-noobs.de.sh

--- mightful-noobs.de.sh
+++ mightful-noobs.de.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-BAKUPDIR=/root/backup
+BACKUPDIR=/root/backup
 HOMEDIR=/root
 FORMAT=$(date +%Y%m%d)-$(date +%H)
 rclone delete --min-age 14d OneDrive:/Backups/mightful-noobs.de
-cd $BAKUPDIR
+cd $BACKUPDIR
 rm *.sql
 mysql -N -e 'show databases' | while read dbname; do mysqldump --complete-insert --routines --triggers --single-transaction "$dbname" > "$dbname".sql; done
 cd /
-rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/crash' --exclude '/var/lib' --exclude '/var/local' --exclude '/var/lock' --exclude '/var/log' --exclude '/var/mail' --exclude '/var/opt' --exclude '/var/run' --exclude '/var/snap' --exclude '/var/tmp' /var $BAKUPDIR
-rsync -a --delete --exclude /home $BAKUPDIR
-rsync -a --delete --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root $BAKUPDIR
-rsync -a --delete /etc $BAKUPDIR
-cd $BAKUPDIR
+rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/crash' --exclude '/var/lib' --exclude '/var/local' --exclude '/var/lock' --exclude '/var/log' --exclude '/var/mail' --exclude '/var/opt' --exclude '/var/run' --exclude '/var/snap' --exclude '/var/tmp' /var $BACKUPDIR
+rsync -a --delete --exclude /home $BACKUPDIR
+rsync -a --delete --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root $BACKUPDIR
+rsync -a --delete /etc $BACKUPDIR
+cd $BACKUPDIR
 tar -zcvf $HOMEDIR/backuputils/upload.tar.gz *
 cd $HOMEDIR/backuputils
 gpg --passphrase-file encryption.txt -c --batch --yes --no-tty upload.tar.gz
