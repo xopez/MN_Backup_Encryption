2b5952c29d2d649873c101da370b13d0a9af0c79
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-03-01 13:02:50 +0100

    Update ts.mightful-noobs.de.sh

--- ts.mightful-noobs.de.sh
+++ ts.mightful-noobs.de.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-BAKUPDIR=/root/backup
+BACKUPDIR=/root/backup
 HOMEDIR=/root
 FORMAT=$(date +%Y%m%d)-$(date +%H)
 rclone delete --min-age 14d OneDrive:/Backups/ts.mightful-noobs.de
 cd /
-rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/crash' --exclude '/var/lib' --exclude '/var/local' --exclude '/var/lock' --exclude '/var/log' --exclude '/var/mail' --exclude '/var/opt' --exclude '/var/run' --exclude '/var/snap' --exclude '/var/tmp' /var $BAKUPDIR
-rsync -a --delete /home $BAKUPDIR
-rsync -a --delete /etc $BAKUPDIR
-rsync -a --delete --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root $BAKUPDIR
-cd $BAKUPDIR
+rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/crash' --exclude '/var/lib' --exclude '/var/local' --exclude '/var/lock' --exclude '/var/log' --exclude '/var/mail' --exclude '/var/opt' --exclude '/var/run' --exclude '/var/snap' --exclude '/var/tmp' /var $BACKUPDIR
+rsync -a --delete /home $BACKUPDIR
+rsync -a --delete /etc $BACKUPDIR
+rsync -a --delete --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root $BACKUPDIR
+cd $BACKUPDIR
 tar -zcvf $HOMEDIR/backuputils/upload.tar.gz *
 cd $HOMEDIR/backuputils
 gpg --passphrase-file encryption.txt -c --batch --yes --no-tty upload.tar.gz
