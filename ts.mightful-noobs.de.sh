6ea3584bcac0c2232b6246a33ca4cb8621fb034f
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-03-20 15:30:08 +0100

    Update ts.mightful-noobs.de.sh

--- ts.mightful-noobs.de.sh
+++ ts.mightful-noobs.de.sh
@@ -2,14 +2,22 @@
 BACKUPDIR=/root/backup
 HOMEDIR=/root
 FORMAT=$(date +%Y%m%d)-$(date +%H)
-rclone delete --min-age 14d OneDrive:/Backups/ts.mightful-noobs.de
-cd /
-rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/crash' --exclude '/var/lib' --exclude '/var/local' --exclude '/var/lock' --exclude '/var/log' --exclude '/var/mail' --exclude '/var/opt' --exclude '/var/run' --exclude '/var/snap' --exclude '/var/tmp' /var $BACKUPDIR
-rsync -a --delete /home $BACKUPDIR
-rsync -a --delete /etc $BACKUPDIR
-rsync -a --delete --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root $BACKUPDIR
-cd $BACKUPDIR
-tar -zcvf $HOMEDIR/backuputils/upload.tar.gz *
-cd $HOMEDIR/backuputils
-gpg --passphrase-file encryption.txt -c --batch --yes --no-tty upload.tar.gz
-rclone copyto upload.tar.gz.gpg OneDrive:/Backups/ts.mightful-noobs.de/$FORMAT.tar.gz.gpg
+echo Deleting old archives...
+rclone delete --min-age 14d SFTP:ts.mightful-noobs.de > /dev/null
+cd / || exit
+echo rsync /var
+rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/lock' --exclude '/var/run' /var "$BACKUPDIR" > /dev/null
+echo rsync /home
+rsync -a --delete /home "$BACKUPDIR" > /dev/null
+echo rsync /root
+rsync -a --delete --exclude '/root/.gnupg' --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root "$BACKUPDIR" > /dev/null
+echo rsync /etc
+rsync -a --delete /etc "$BACKUPDIR" > /dev/null
+echo Packing...
+cd "$BACKUPDIR" || exit
+tar -zcvf "$HOMEDIR"/backuputils/upload.tar.gz ./* > /dev/null
+echo Encrypting...
+cd "$HOMEDIR"/backuputils || exit
+gpg --passphrase-file encryption.txt -c --batch --yes --no-tty upload.tar.gz > /dev/null
+echo Uploading...
+rclone copyto upload.tar.gz.gpg SFTP:ts.mightful-noobs.de/"$FORMAT".tar.gz.gpg > /dev/null
