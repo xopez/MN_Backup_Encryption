4951408aa6519c41a139ef5d3ce5f6805c72b070
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-03-20 15:29:54 +0100

    Update mightful-noobs.de.sh

--- mightful-noobs.de.sh
+++ mightful-noobs.de.sh
@@ -2,17 +2,26 @@
 BACKUPDIR=/root/backup
 HOMEDIR=/root
 FORMAT=$(date +%Y%m%d)-$(date +%H)
-rclone delete --min-age 14d OneDrive:/Backups/mightful-noobs.de
-cd $BACKUPDIR
-rm *.sql
-mysql -N -e 'show databases' | while read dbname; do mysqldump --complete-insert --routines --triggers --single-transaction "$dbname" > "$dbname".sql; done
-cd /
-rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/crash' --exclude '/var/lib' --exclude '/var/local' --exclude '/var/lock' --exclude '/var/log' --exclude '/var/mail' --exclude '/var/opt' --exclude '/var/run' --exclude '/var/snap' --exclude '/var/tmp' /var $BACKUPDIR
-rsync -a --delete --exclude /home $BACKUPDIR
-rsync -a --delete --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root $BACKUPDIR
-rsync -a --delete /etc $BACKUPDIR
-cd $BACKUPDIR
-tar -zcvf $HOMEDIR/backuputils/upload.tar.gz *
-cd $HOMEDIR/backuputils
-gpg --passphrase-file encryption.txt -c --batch --yes --no-tty upload.tar.gz
-rclone copyto upload.tar.gz.gpg OneDrive:/Backups/mightful-noobs.de/$FORMAT.tar.gz.gpg
+echo Deleting old archives...
+rclone delete --min-age 14d SFTP:mightful-noobs.de > /dev/null
+cd "$BACKUPDIR" || exit
+echo MariaDB Backup
+rm ./*.sql
+mysql -N -e 'show databases' | while read -r dbname; do mysqldump --complete-insert --routines --triggers --single-transaction "$dbname" > "$dbname".sql; done
+cd / || exit
+echo rsync /var
+rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/lock' --exclude '/var/run' /var "$BACKUPDIR" > /dev/null
+echo rsync /home
+rsync -a --delete /home "$BACKUPDIR" > /dev/null
+echo rsync /root
+rsync -a --delete --exclude '/root/.gnupg' --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root "$BACKUPDIR" > /dev/null
+echo rsync /etc
+rsync -a --delete /etc "$BACKUPDIR" > /dev/null
+echo Packing...
+cd "$BACKUPDIR" || exit
+tar -zcvf "$HOMEDIR"/backuputils/upload.tar.gz ./* > /dev/null
+echo Encrypting...
+cd "$HOMEDIR"/backuputils || exit
+gpg --passphrase-file encryption.txt -c --batch --yes --no-tty upload.tar.gz > /dev/null
+echo Uploading...
+rclone copyto upload.tar.gz.gpg SFTP:mightful-noobs.de/"$FORMAT".tar.gz.gpg > /dev/null
