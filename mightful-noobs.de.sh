96b92134ef431f5495e59d6753b4c81c8de9859a
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-05-03 13:26:39 +0200

    Update mightful-noobs.de.sh

--- mightful-noobs.de.sh
+++ mightful-noobs.de.sh
@@ -6,13 +6,14 @@ echo Deleting old archives...
 rclone delete --min-age 14d SFTP:mightful-noobs.de > /dev/null
 cd "$BACKUPDIR" || exit
 echo MariaDB Backup
-rm ./*.sql
+count=$(find ./*.sql 2>/dev/null | wc -l)
+if [ "$count" != 0 ]; then
+	rm ./*.sql
+fi
 mysql -N -e 'show databases' | while read -r dbname; do mysqldump --complete-insert --routines --triggers --single-transaction "$dbname" > "$dbname".sql; done
 cd / || exit
 echo rsync /var
-rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/lock' --exclude '/var/run' /var "$BACKUPDIR" > /dev/null
-echo rsync /home
-rsync -a --delete /home "$BACKUPDIR" > /dev/null
+rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/local' --exclude '/var/lock' --exclude '/var/lib' --exclude '/var/log' --exclude '/var/mail' --exclude '/var/spool/mail' --exclude '/var/opt' --exclude '/var/run' --exclude '/var/tmp' /var "$BACKUPDIR" > /dev/null
 echo rsync /root
 rsync -a --delete --exclude '/root/.gnupg' --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root "$BACKUPDIR" > /dev/null
 echo rsync /etc
