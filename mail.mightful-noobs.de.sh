a86e052a2a2001844afd4d9db31c0610dbf6eb31
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-06-20 06:13:51 +0200

    Update mail.mightful-noobs.de.sh

--- mail.mightful-noobs.de.sh
+++ mail.mightful-noobs.de.sh
@@ -3,15 +3,23 @@ BACKUPDIR=/root/backup
 HOMEDIR=/root
 FORMAT=$(date +%Y%m%d)-$(date +%H)
 export MAILCOW_BACKUP_LOCATION=$BACKUPDIR/mailcow
-rclone delete --min-age 14d Nextcloud:/Backups/mail.mightful-noobs.de
-cd /opt/mailcow-dockerized
-helper-scripts/backup_and_restore.sh backup --delete-days 0 all
-cd /
-rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/crash' --exclude '/var/lib' --exclude '/var/local' --exclude '/var/lock' --exclude '/var/log' --exclude '/var/mail' --exclude '/var/opt' --exclude '/var/run' --exclude '/var/snap' --exclude '/var/tmp' /var $BACKUPDIR
-rsync -a --delete --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root $BACKUPDIR
-rsync -a --delete /etc $BACKUPDIR
-cd $BACKUPDIR
-tar -zcvf $HOMEDIR/backuputils/upload.tar.gz *
-cd $HOMEDIR/backuputils
-gpg --passphrase-file encryption.txt -c --batch --yes --no-tty upload.tar.gz
-rclone copyto upload.tar.gz.gpg Nextcloud:/Backups/mail.mightful-noobs.de/$FORMAT.tar.gz.gpg
+echo Deleting old archives...
+rclone delete --min-age 14d SFTP:mail.mightful-noobs.de > /dev/null
+echo Mailcow Backup...
+cd /opt/mailcow-dockerized  || exit
+helper-scripts/backup_and_restore.sh backup --delete-days 0 all  > /dev/null
+cd / || exit
+echo rsync /var
+rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/local' --exclude '/var/lock' --exclude '/var/lib' --exclude '/var/log' --exclude '/var/mail' --exclude '/var/spool/mail' --exclude '/var/opt' --exclude '/var/run' --exclude '/var/tmp' /var "$BACKUPDIR" > /dev/null
+echo rsync /root
+rsync -a --delete --exclude '/root/.gnupg' --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root "$BACKUPDIR" > /dev/null
+echo rsync /etc
+rsync -a --delete /etc "$BACKUPDIR" > /dev/null
+echo Packing...
+cd "$BACKUPDIR" || exit
+tar -zcvf "$HOMEDIR"/backuputils/upload.tar.gz ./* > /dev/null
+echo Encrypting...
+cd "$HOMEDIR"/backuputils || exit
+gpg --passphrase-file encryption.txt -c --batch --yes --no-tty upload.tar.gz > /dev/null
+echo Uploading...
+rclone copyto upload.tar.gz.gpg SFTP:mail.mightful-noobs.de/"$FORMAT".tar.gz.gpg > /dev/null
