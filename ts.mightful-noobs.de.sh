2a8a8472c6835ff569429215c4db5b795732c49b
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-09-16 19:36:42 +0200

    resort

--- ts.mightful-noobs.de.sh
+++ ts.mightful-noobs.de.sh
@@ -5,14 +5,16 @@ FORMAT=$(date +%Y%m%d)-$(date +%H)
 echo Deleting old archives...
 rclone delete --min-age 14d SFTP:ts.mightful-noobs.de > /dev/null
 cd / || exit
-echo rsync /var
-rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/local' --exclude '/var/lock' --exclude '/var/lib' --exclude '/var/log' --exclude '/var/mail' --exclude '/var/spool/mail' --exclude '/var/opt' --exclude '/var/run' --exclude '/var/tmp' /var "$BACKUPDIR" > /dev/null
 echo rsync /home
 rsync -a --delete /home "$BACKUPDIR" > /dev/null
-echo rsync /root
-rsync -a --delete --exclude '/root/.gnupg' --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root "$BACKUPDIR" > /dev/null
 echo rsync /etc
 rsync -a --delete /etc "$BACKUPDIR" > /dev/null
+echo rsync /root
+rsync -a --delete --exclude '/root/.gnupg' --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root "$BACKUPDIR" > /dev/null
+echo rsync /usr
+rsync -a --delete --exclude '/usr/bin' --exclude '/usr/games' --exclude '/usr/include' --exclude '/usr/lib*' --exclude '/usr/local' --exclude '/usr/sbin' --exclude '/usr/src' /usr "$BACKUPDIR" > /dev/null
+echo rsync /var
+rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/local' --exclude '/var/lock' --exclude '/var/lib' --exclude '/var/log' --exclude '/var/mail' --exclude '/var/spool/mail' --exclude '/var/opt' --exclude '/var/run' --exclude '/var/tmp' /var "$BACKUPDIR" > /dev/null
 echo Packing...
 cd "$BACKUPDIR" || exit
 tar -zcvf "$HOMEDIR"/backuputils/upload.tar.gz ./* > /dev/null
