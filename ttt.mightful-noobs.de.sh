d76ea6f7a52ed62337a62ff48178481b9672bb0e
Sascha Moser <28950736+xopez@users.noreply.github.com>
2023-11-07 19:20:26 +0100

    Optimize backup

--- ttt.mightful-noobs.de.sh
+++ ttt.mightful-noobs.de.sh
@@ -1,27 +1,74 @@
 #!/bin/bash
-BACKUPDIR=/root/backup
-HOMEDIR=/root
+
+# Verzeichnisse und Dateinamen definieren
+BACKUPDIR="/root/backup"
+HOMEDIR="/root"
 FORMAT=$(date +%Y%m%d)-$(date +%H)
-echo Deleting old archives...
-rclone delete --min-age 14d SFTP:ttt.mightful-noobs.de > /dev/null
-cd /
-echo rsync /home
-rsync -a --delete --exclude '/home/gmodserver/.steam' --exclude '/home/gmodserver/.local' --exclude '/home/gmodserver/css' --exclude '/home/gmodserver/serverfiles/bin' --exclude '/home/gmodserver/serverfiles/platform' --exclude '/home/gmodserver/serverfiles/sourceengine' --exclude '/home/gmodserver/serverfiles/steamapps' --exclude '/home/gmodserver/serverfiles/steam_cache' --exclude '/home/gmodserver/serverfiles/garrysmod/*.vpk' --exclude '/home/gmodserver/serverfiles/garrysmod/*.pack' --exclude '/home/gmodserver/serverfiles/garrysmod/cache' /home "$BACKUPDIR" > /dev/null
-echo rsync /etc
-rsync -a --delete /etc "$BACKUPDIR" > /dev/null
-echo rsync /root
-rsync -a --delete --exclude '/root/.gnupg' --exclude '/root/backuputils/upload.tar.g*' --exclude '/root/backup' /root "$BACKUPDIR" > /dev/null
-echo rsync /usr
-rsync -a --delete --exclude '/usr/bin' --exclude '/usr/games' --exclude '/usr/include' --exclude '/usr/lib*' --exclude '/usr/local' --exclude '/usr/sbin' --exclude '/usr/src' /usr "$BACKUPDIR" > /dev/null
-echo rsync /var
-rsync -a --delete --exclude '/var/backups' --exclude '/var/cache' --exclude '/var/local' --exclude '/var/lock' --exclude '/var/lib' --exclude '/var/log' --exclude '/var/mail' --exclude '/var/spool/mail' --exclude '/var/opt' --exclude '/var/run' --exclude '/var/tmp' /var "$BACKUPDIR" > /dev/null
-echo Packing...
+
+# Funktion zur Durchführung des Backups
+perform_backup() {
+  SOURCE_DIR="$1"
+  shift
+  EXCLUDE_OPTIONS=("$@")
+
+  echo "Backup von $SOURCE_DIR erstellen..."
+  rsync -a --delete "${EXCLUDE_OPTIONS[@]}" "$SOURCE_DIR" "$BACKUPDIR" > /dev/null
+}
+
+# Alte Archive löschen
+echo "Lösche alte Archive..."
+rclone delete --min-age 30d SFTP:ttt.mightful-noobs.de > /dev/null
+
+# Backup von /home erstellen
+HOME_EXCLUDES=(
+  --exclude '/home/ttt/.local' 
+  --exclude '/home/ttt/css' 
+  --exclude '/home/ttt/serverfiles/bin' 
+  --exclude '/home/ttt/serverfiles/sourceengine' 
+  --exclude '/home/ttt/serverfiles/steamapps' 
+  --exclude '/home/ttt/serverfiles/steam_cache' 
+  --exclude '/home/ttt/serverfiles/garrysmod/*.vpk' 
+  --exclude '/home/ttt/serverfiles/garrysmod/cache'
+)
+
+perform_backup "/home" "${HOME_EXCLUDES[@]}"
+
+# Backup von /etc erstellen
+ETC_EXCLUDES=()  # Hier ggf. die passenden Exclude-Optionen für /etc einfügen
+
+perform_backup "/etc" "${ETC_EXCLUDES[@]}"
+
+# Backup von /root erstellen
+ROOT_EXCLUDES=(
+  --exclude '/root/.gnupg' 
+  --exclude '/root/backuputils/upload.tar.g*' 
+  --exclude '/root/backup'
+)
+
+perform_backup "/root" "${ROOT_EXCLUDES[@]}"
+# Backup von /var erstellen
+VAR_EXCLUDES=(
+  --exclude '/var/cache' 
+  --exclude '/var/lock' 
+  --exclude '/var/lib' 
+  --exclude '/var/mail' 
+  --exclude '/var/run' 
+)
+
+perform_backup "/var" "${VAR_EXCLUDES[@]}"
+
+# Archiv packen
+echo "Packen..."
 cd "$BACKUPDIR" || exit
 tar -zcvf "$HOMEDIR"/backuputils/upload.tar.gz ./* > /dev/null
-echo Encrypting...
+
+# Verschlüsseln
+echo "Verschlüsseln..."
 cd "$HOMEDIR"/backuputils || exit
 gpg --passphrase-file encryption.txt -c --batch --yes --no-tty upload.tar.gz > /dev/null
 rm upload.tar.gz
-echo Uploading...
+
+# Hochladen
+echo "Hochladen..."
 rclone copyto upload.tar.gz.gpg SFTP:ttt.mightful-noobs.de/"$FORMAT".tar.gz.gpg > /dev/null
 rm upload.tar.gz.gpg
